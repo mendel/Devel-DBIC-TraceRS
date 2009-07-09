b4a3a8d64e133251c6fd7c8b2490b2c3187376e0
Norbert Buchmuller <norbi@nix.hu>
2009-07-09 08:51:47 +0200

    Various improvements.
    Does no eval but temporarily wraps $schema->throw_exception.
    Allows modules to import their stuff into its namespace.
    Wraps new() as well.

--- lib/DBIx/Class/RememberSearch.pm
+++ lib/DBIx/Class/RememberSearch.pm
@@ -5,10 +5,10 @@ use warnings;
 
 use base qw(DBIx::Class);
 
-use Devel::StackTrace ();
-use Sub::Name ();
-use Devel::Symdump ();
-use Scalar::Util ();
+use Devel::StackTrace;
+use Sub::Name;
+use Devel::Symdump;
+use Scalar::Util;
 
 use DBIx::Class::ResultSet;
 
@@ -25,9 +25,36 @@ DBIx::Class::ResultSet->mk_group_accessors(simple => qw(_search_stacktrace));
   my @search_methods =
     grep { /^DBIx::Class::ResultSet::.*search.*/ } @all_methods;
 
+  # wrap new() so that $schema->resultset(...) is remembered
+  foreach my $method (qw(DBIx::Class::ResultSet::new)) {
+    my $orig_method = \&$method;
+    *$method = subname $method => sub {
+      my $class = shift;
+
+      my (@ret, $self);
+      if (wantarray) {
+        @ret = $class->$orig_method(@_);
+      } elsif (defined wantarray) {
+        $self = $class->$orig_method(@_);
+      } else {
+        $class->$orig_method(@_);
+      }
+
+      $self->_append_to_search_stacktrace if $self;
+
+      return
+        wantarray
+          ? @ret
+          : defined wantarray
+            ? $self
+            : ();
+    };
+  }
+
+  # wrap all search methods so that they are remembered
   foreach my $method (@search_methods) {
     my $orig_method = \&$method;
-    *$method = Sub::Name::subname $method => sub {
+    *$method = subname $method => sub {
       my $self = shift;
 
       my (@ret, $ret);
@@ -39,14 +66,7 @@ DBIx::Class::ResultSet->mk_group_accessors(simple => qw(_search_stacktrace));
         $self->$orig_method(@_);
       }
 
-      #FIXME append only once the same caller info (b/c eg. search calls search_rs, so the outer caller info is added twice)
-      $ret->_search_stacktrace([
-        @{$self->_search_stacktrace || []},
-        Devel::StackTrace->new(
-          frame_filter => sub { shift->{caller}->[0] !~ /^DBIx::Class/ },
-          no_refs => 1,
-        ),
-      ]);
+      $self->_append_to_search_stacktrace;
 
       return
         wantarray
@@ -57,38 +77,38 @@ DBIx::Class::ResultSet->mk_group_accessors(simple => qw(_search_stacktrace));
     };
   }
 
+  # wrap all methods to rewrite exceptions thrown from them
   foreach my $method (@all_methods) {
     my $orig_method = \&$method;
-    *$method = Sub::Name::subname $method => sub {
+    *$method = subname $method => sub {
       my $self = shift;
 
-      warn caller(2);
-      my (@ret, $ret);
-      if (wantarray) {
-        @ret = eval { $self->$orig_method(@_); };
-      } elsif (defined wantarray) {
-        $ret = eval { $self->$orig_method(@_); };
-      } else {
-        eval { $self->$orig_method(@_); };
-      }
-      if ($@ ne '') {
-        #FIXME cleanly append to the message (probably wrapped in an exception object)
-        #FIXME append only once
-        if (Scalar::Util::blessed $self) {
-          die join("\n", $@, map { $_->as_string } @{$self->_search_stacktrace});
-        } else {
-          die;  # propagate
-        }
-      }
+      my $orig_throw_exception = \&DBIx::Class::Schema::throw_exception;
+      local *DBIx::Class::Schema::throw_exception = subname throw_exception => sub {
+        my $schema = shift;
 
-      return
-        wantarray
-          ? @ret
-          : defined wantarray
-            ? $ret
-            : ();
+        $_[0] = join("\n", $_[0], map { $_->as_string } @{$self->_search_stacktrace})
+          if Scalar::Util::blessed $self;
+
+        return $schema->$orig_throw_exception(@_);
+      };
+
+      return $self->$orig_method(@_);
     };
   }
+
+  *DBIx::Class::ResultSet::_append_to_search_stacktrace = subname _append_to_search_stacktrace => sub {
+    my $self = shift;
+
+    #FIXME append only once the same caller info (b/c eg. search calls search_rs, so the outer caller info is added twice)
+    $self->_search_stacktrace([
+      @{$self->_search_stacktrace || []},
+      Devel::StackTrace->new(
+        frame_filter => sub { shift->{caller}->[0] !~ /^DBIx::Class/ },
+        no_refs => 1,
+      ),
+    ]);
+  };
 }
 
 1;
